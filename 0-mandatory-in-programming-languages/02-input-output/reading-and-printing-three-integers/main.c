2afb46db5d1b547015772209b9a0289a98b33f08
joaovictorvns <jv369t7d2hx@gmail.com>
2023-07-01 23:48:41 -0300

    Adding the reading-and-printing-three-integers project

---
+++ 0-mandatory-in-programming-languages/02-input-output/reading-and-printing-three-integers/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%i %i %i", &a, &b, &c);
+    printf("firt integer: %i\n", a);
+    printf("second integer: %i\n", b);
+    printf("third integer: %i\n", c);
+    return 0;
+}
